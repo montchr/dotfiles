59f20588886941409fdeabd1e1d45e44316e6759
Chris Montgomery <mont.chr@gmail.com>
2014-03-02 02:18:09 -0500

    git: default ga adds untracked

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,7 +7,7 @@ then
 fi
 
 # The rest of my fun git aliases
-alias ga='git add'
+alias ga='git add --all'
 alias gaa='git all'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
