dee5c57b5e0f1ae4644e3054089a64e5da1ddb29
Chris Montgomery <chris@montchr.io>
2014-03-03 18:02:19 -0500

    add flushdns alias

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -19,6 +19,9 @@ alias -- -="cd -"
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ipconfig getifaddr en1"
 
+# Flush DNS cache
+alias flushdns="dscacheutil -flushcache"
+
 # Empty the Trash on all mounted volumes and the main HDD
 # Also, clear Apple’s System Logs to improve shell startup speed
 alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl"
