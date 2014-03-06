88747a89d47f28167c9452d7af6b9f9558fac31b
Chris Montgomery <chris@montchr.io>
2014-03-06 10:39:57 -0500

    derp. install oh-my-zsh.

---
+++ zsh/install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Install oh-my-zsh if it's not already installed
+if ! [ -d $HOME/.oh-my-zsh ]
+  then
+  e_header "installing oh-my-zsh"
+  curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+  [[ $? ]] && e_success "Done"
+fi
