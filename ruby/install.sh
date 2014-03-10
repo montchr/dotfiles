39373a34415a63cf73d55998f46a281b095ac82a
Chris Montgomery <chris@montchr.io>
2014-03-10 16:11:50 -0400

    install ruby 2.1.0, not 1.9.3

--- ruby/install.sh
+++ ruby/install.sh
@@ -3,5 +3,5 @@
 if test ! $(which rvm)
 then
   echo "  Installing rvm for you."
-  \curl -sSL https://get.rvm.io | bash -s stable --ruby=1.9.3 > /tmp/rvm-install.log
+  \curl -sSL https://get.rvm.io | bash -s stable --ruby=2.1.0 > /tmp/rvm-install.log
 fi
