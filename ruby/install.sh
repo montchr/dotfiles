c6577a98b6a0bfc2d67cf189119b6c5e88ae814f
Chris Montgomery <chris@montchr.io>
2014-03-10 22:27:45 -0400

    rb: 2.1.0 default, install global gems

--- ruby/install.sh
+++ ruby/install.sh
@@ -5,3 +5,7 @@ then
   echo "  Installing rvm for you."
   \curl -sSL https://get.rvm.io | bash -s stable --ruby=2.1.0 > /tmp/rvm-install.log
 fi
+
+rvm --default use 2.1.0
+
+rvm @global do gem install bundler rake
