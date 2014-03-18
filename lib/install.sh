63677034522e0e5e381a9477da868115ba539227
Chris Montgomery <chris@montchr.io>
2014-03-17 20:13:06 -0400

    install fixes, todo additions

--- lib/install.sh
+++ lib/install.sh
@@ -3,4 +3,6 @@ export DOTFILES=$HOME/.dotfiles
 # Link XKCD Password Generator to ./bin
 # http://xkcd.com/936/
 # https://github.com/redacted/XKCD-password-generator
-ln -s $DOTFILES/lib/XKCD-password-generator/xkcd_password.py ~/.dotfiles/bin/xkcdpasswd
+if ! [ -f "$HOME/.dotfiles/bin/xkcdpasswd" ]; then
+  ln -s "$DOTFILES/lib/XKCD-password-generator/xkcd_password.py" ~/.dotfiles/bin/xkcdpasswd
+fi
