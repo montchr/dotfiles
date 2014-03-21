8f6d87418f33b1c2048c2958ed0b627e55cbc3d9
Chris Montgomery <chris@montchr.io>
2014-03-21 18:18:13 -0400

    xkcdp: don't check for file, check for anything
    The conditional `[ -f "$HOME/.dotfiles/bin/xkcdpasswd" ]` will always
    fail because xkcdpasswd is symlinked to bin, it's not a file.

--- lib/install.sh
+++ lib/install.sh
@@ -3,6 +3,6 @@ export DOTFILES=$HOME/.dotfiles
 # Link XKCD Password Generator to ./bin
 # http://xkcd.com/936/
 # https://github.com/redacted/XKCD-password-generator
-if ! [ -f "$HOME/.dotfiles/bin/xkcdpasswd" ]; then
+if ! [ -a "$HOME/.dotfiles/bin/xkcdpasswd" ]; then
   ln -s "$DOTFILES/lib/XKCD-password-generator/xkcd_password.py" ~/.dotfiles/bin/xkcdpasswd
 fi
