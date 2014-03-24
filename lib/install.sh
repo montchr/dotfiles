8712aad5da5e81918bba731bd83977f291b30c0f
Chris Montgomery <chris@montchr.io>
2014-03-24 02:38:04 -0400

    sh: don't forget the hash bang

--- lib/install.sh
+++ lib/install.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
+
 export DOTFILES=$HOME/.dotfiles
 
 # Link XKCD Password Generator to ./bin
 # http://xkcd.com/936/
 # https://github.com/redacted/XKCD-password-generator
-ln -fs "$DOTFILES/lib/XKCD-password-generator/xkcd_password.py" ~/.dotfiles/bin/xkcdpasswd
+ln -fs "$DOTFILES/lib/XKCD-password-generator/xkcd_password.py" "$DOTFILES/bin/xkcdpasswd"
