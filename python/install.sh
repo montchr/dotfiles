19d7b650590f1e6846bc530d7478bf2840c8bcf0
Chris Montgomery <chris@montchr.io>
2014-03-24 13:25:46 -0400

    py: remove --user from powerline install

--- python/install.sh
+++ python/install.sh
@@ -1,5 +1,5 @@
 # Install Powerline, for ultimate prompt coolness
-pip install --user git+git://github.com/Lokaltog/powerline
+pip install git+git://github.com/Lokaltog/powerline
 
 pip install SpoofMAC
 pip install beets
