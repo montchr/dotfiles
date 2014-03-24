e3e5cde927ab06722f6c105f23a3fc27765d029a
Chris Montgomery <chris@montchr.io>
2014-03-24 13:22:21 -0400

    rb: use 2.1.1

--- ruby/install.sh
+++ ruby/install.sh
@@ -3,9 +3,9 @@
 if test ! $(which rvm)
 then
   echo "  Installing rvm for you."
-  \curl -sSL https://get.rvm.io | bash -s stable --ruby=2.1.0 > /tmp/rvm-install.log
+  \curl -sSL https://get.rvm.io | bash -s stable --ruby=2.1.1 > /tmp/rvm-install.log
 fi
 
-rvm --default use 2.1.0
+rvm --default use 2.1.1
 
 rvm @global do gem install bundler rake
