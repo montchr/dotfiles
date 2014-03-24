47f290eaa26f2e038e0c67128fb9594a61ac9af1
Chris Montgomery <chris@montchr.io>
2014-03-23 23:22:28 -0400

    lib: force symlink xkcdp

--- lib/install.sh
+++ lib/install.sh
@@ -3,6 +3,4 @@ export DOTFILES=$HOME/.dotfiles
 # Link XKCD Password Generator to ./bin
 # http://xkcd.com/936/
 # https://github.com/redacted/XKCD-password-generator
-if ! [ -a "$HOME/.dotfiles/bin/xkcdpasswd" ]; then
-  ln -s "$DOTFILES/lib/XKCD-password-generator/xkcd_password.py" ~/.dotfiles/bin/xkcdpasswd
-fi
+ln -fs "$DOTFILES/lib/XKCD-password-generator/xkcd_password.py" ~/.dotfiles/bin/xkcdpasswd
