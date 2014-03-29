b29ab180e50016c8d0e9e825e22fd0103a6e5fd5
Chris Montgomery <chris@montchr.io>
2014-03-29 14:30:45 -0400

    git: gd and gdl aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -12,7 +12,7 @@ alias gaa='git all'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
-alias gd='git diff'
+alias gd='git difftool'
 alias go='git checkout'
 alias gc='git commit'
 alias gca='git commit -a'
@@ -21,3 +21,6 @@ alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
            perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
+# Show list of files changed in a commit
+# Follow with commit hash
+alias gdl="git diff-tree --no-commit-id --name-only -r $1"
