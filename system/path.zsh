dd644482163748be3be3890abdc2edfa88f515ef
Chris Montgomery <chris@montchr.io>
2014-03-29 14:32:31 -0400

    brew: install php56 and composer

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:$HOME/bin:$HOME/.rvm/bin:$HOME/.wp-cli/bin:$HOME/.cabal/bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
+export PATH="./bin:$HOME/bin:$HOME/.rvm/bin:$HOME/.wp-cli/bin:$HOME/.cabal/bin:$(brew --prefix josegonzalez/php/php56)/bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
