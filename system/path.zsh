3946c048a521dace061fe6e8053af9bf5dfe4d7c
Chris Montgomery <chris@montchr.io>
2014-05-29 16:20:29 -0400

    php: fix composer and php installs

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:$DOTFILES/bin:$HOME/bin:$HOME/.rvm/bin:$HOME/.wp-cli/bin:$HOME/.cabal/bin:$(brew --prefix josegonzalez/php/php56)/bin:/usr/local/bin:/usr/local/sbin:$PATH"
+export PATH="./bin:$DOTFILES/bin:$HOME/bin:$HOME/.rvm/bin:$HOME/.wp-cli/bin:$HOME/.cabal/bin:$(brew --prefix php55)/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
