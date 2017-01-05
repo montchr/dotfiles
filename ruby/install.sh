5b450d44ee8bd6e53129ab56a92333ee14fc3a0c
Chris Montgomery <chris@montchr.io>
2017-01-05 17:19:48 -0500

    don't install an ancient version of ruby when installing rvm

--- ruby/install.sh
+++ ruby/install.sh
@@ -3,9 +3,7 @@
 if test ! $(which rvm)
 then
   echo "  Installing rvm for you."
-  \curl -sSL https://get.rvm.io | bash -s stable --ruby=2.1.2 > /tmp/rvm-install.log
+  \curl -sSL https://get.rvm.io | bash -s stable --ruby > /tmp/rvm-install.log
 fi
-
-rvm --default use 2.1.2
 
 rvm @global do gem install bundler rake timetrap doing
