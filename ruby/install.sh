492010e70f7eb9fe9c2842b872efe68b857a6f4f
Chris Montgomery <chris@montchr.io>
2017-01-05 17:26:29 -0500

    add rvm to bash path
    Since our zsh config isn't set up yet, rvm needs to run by adding to
    path in a bash dotfile.

--- ruby/install.sh
+++ ruby/install.sh
@@ -3,7 +3,7 @@
 if test ! $(which rvm)
 then
   echo "  Installing rvm for you."
-  \curl -sSL https://get.rvm.io | bash -s stable --ruby > /tmp/rvm-install.log
+  \curl -sSL https://get.rvm.io | bash -s stable --ruby --auto-dotfiles > /tmp/rvm-install.log
 fi
 
 rvm @global do gem install bundler rake timetrap doing
