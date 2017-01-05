9d536ece9edab78864e0e34276e7e199c9e30118
Chris Montgomery <chris@montchr.io>
2017-01-05 17:46:10 -0500

    use php7.1 bin in PATH

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
 export PATH="./bin:$DOTFILES/bin:$HOME/bin:$HOME/.rvm/bin:$HOME/.wp-cli/bin:$HOME/.cabal/bin:/usr/local/bin:/usr/local/sbin:$PATH"
-export PATH="$(brew --prefix homebrew/php/php70)/bin:$PATH"
+export PATH="$(brew --prefix homebrew/php/php71)/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
