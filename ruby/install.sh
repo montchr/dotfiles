643cb6263be1357a1ab202c3194146c34f5c2d88
Chris Montgomery <chris@montchr.io>
2017-12-04 14:19:42 -0500

    Fully set up rbenv -- rubies must be installed

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
+# Setup rbenv
+rbenv init
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash
+# Install latest stable Ruby
+# https://gist.github.com/rgb-one/6cc6b0a2e609232e8038
+LATEST_RUBY_VERSION=$(rbenv install -l | grep -v - | tail -1)
+rbenv install -v $LATEST_RUBY_VERSION
+rbenv global $LATEST_RUBY_VERSION
+
+gem install sqlite3
 gem install bundler
 bundle install
