9bffa9f40a9e3fef2e49ec71a5bdd857af07a45b
Chris Montgomery <chris@montchr.io>
2018-02-21 14:38:38 -0500

    Add a PHP path file

---
+++ php/path.zsh
@@ -0,0 +1,4 @@
+# PHP 7.1
+export PATH="$(brew --prefix homebrew/php/php71)/bin:$PATH"
+
+# export PATH=""
--- system/path.zsh
+++ system/path.zsh
@@ -1,6 +1,5 @@
 export PATH="./bin:$DOTFILES/bin:$HOME/bin:$HOME/.wp-cli/bin:$HOME/.cabal/bin:$PATH"
-# PHP 7.1
-export PATH="$(brew --prefix homebrew/php/php71)/bin:$PATH"
+
 # Maid
 export PATH="$PATH:$HOME/.maid/bin"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
