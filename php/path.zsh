8e65ab2336a109f8061f4d9776ff79dd9e27a30c
Chris Montgomery <chris@montchr.io>
2019-02-12 12:07:12 -0500

    Use PHP 7.1

--- php/path.zsh
+++ php/path.zsh
@@ -1,7 +1,7 @@
 # PHP 7.0
-export PATH="$(brew --prefix php@7.0)/bin:$PATH"
+# export PATH="$(brew --prefix php@7.0)/bin:$PATH"
 
 # PHP 7.1
-# export PATH="$(brew --prefix php@7.1)/bin:$PATH"
+export PATH="$(brew --prefix php@7.1)/bin:$PATH"
 
 export PATH="./vendor/bin:$PATH"
