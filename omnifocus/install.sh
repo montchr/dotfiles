4cb09559417d2db0079529fc9a360972e13b7a0a
Chris Montgomery <chris@alley.co>
2019-04-07 23:21:27 -0400

    Check to see if omnifocus/perspective-icons exists before cloning

--- omnifocus/install.sh
+++ omnifocus/install.sh
@@ -1,1 +1,5 @@
-git clone https://github.com/deaghean/omnifocus-perspective-icons.git $DOTFILES/omnifocus/perspective-icons
+#!/bin/sh
+
+if [ ! -d "$DOTFILES/omnifocus/perspective-icons" ]; then
+  git clone https://github.com/deaghean/omnifocus-perspective-icons.git $DOTFILES/omnifocus/perspective-icons
+fi
