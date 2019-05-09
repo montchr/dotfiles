37583509163dcdc1c9c93b67d6c35575acde7053
Chris Montgomery <chris@montchr.io>
2019-05-08 20:07:52 -0400

    Install the latest version of Beets from GitHub

--- beets/install.sh
+++ beets/install.sh
@@ -2,8 +2,19 @@
 
 export DOTFILES=$HOME/.dotfiles
 
-# Beets
-pip install beets
+# Install the latest version of Beets from GitHub
+BEETS_SRC_DIR=$DOTFILES/beets/beets
+pip uninstall beets
+if [ -d $BEETS_SRC_DIR ]; then
+  cd $BEETS_SRC_DIR
+  git pull
+else
+  git clone https://github.com/beetbox/beets.git $BEETS_SRC_DIR
+  cd $BEETS_SRC_DIR
+fi
+pip install -e .
+cd $DOTFILES
+
 pip install discogs-client
 pip install beets-bandcamp
 # Beets: LastGenre
