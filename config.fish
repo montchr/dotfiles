b86fb9fd9ff28d327ee24a590d814f51b64556c7
Chris Montgomery <chris@alley.co>
2019-12-20 11:23:06 -0500

    Use $DOTFILES!!!

--- config.fish
+++ config.fish
@@ -1,19 +1,20 @@
-set -x -g TERM "xterm-256color"
+set -xg TERM "xterm-256color"
+set -xg DOTFILES "$HOME/.dotfiles"
 
-for env_file in */env.fish; source $env_file; end
-for path_file in */path.fish; source $path_file; end
-for alias_file in */aliases.fish; source $alias_file; end
+for env_file in $DOTFILES/*/env.fish; source $env_file; end
+for path_file in $DOTFILES/*/path.fish; source $path_file; end
+for alias_file in $DOTFILES/*/aliases.fish; source $alias_file; end
 
-for completion_dir in fish/*/completions
+for completion_dir in $DOTFILES/fish/*/completions
     set fish_complete_path $completion_dir $fish_complete_path
 end
-for completion_dir in */completions
+for completion_dir in $DOTFILES/*/completions
     set fish_complete_path $completion_dir $fish_complete_path
 end
 
-for func_dir in fish/*/functions
+for func_dir in $DOTFILES/fish/*/functions
     set fish_function_path $func_dir $fish_function_path
 end
-for func_dir in */functions
+for func_dir in $DOTFILES/*/functions
     set fish_function_path $func_dir $fish_function_path
 end
