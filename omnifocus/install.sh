537affbcc15104df76fa8a1be7804af7ead5c585
Chris Montgomery <chris@alley.co>
2019-12-21 09:19:07 -0500

    no more omnifocus

--- omnifocus/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-if [ ! -d "$DOTFILES/omnifocus/perspective-icons" ]; then
-  git clone https://github.com/deaghean/omnifocus-perspective-icons.git $DOTFILES/omnifocus/perspective-icons
-fi
