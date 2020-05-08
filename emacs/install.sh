ff3ca95263ae84c296aed6ed5bd5fce90529ef78
Chris Montgomery <chris@alley.co>
2020-05-08 12:26:48 -0400

    Use Prelude

--- emacs/install.sh
+++ emacs/install.sh
@@ -1,7 +1,12 @@
-#!/usr/env bash
+#!/bin/bash
 
 emacs_dir="$HOME/.emacs.d"
 
-if [ ! -d emacs_dir ] && [ ! -L emacs_dir ]; then
-    ln -s "$DOTFILES/emacs/emacs.d" "$emacs_dir"
+# Install Prelude to ~/.emacs.d
+if [ ! -d $emacs_dir ] && [ ! -L $emacs_dir ]; then
+  export PRELUDE_INSTALL_DIR="$DOTFILES/emacs/emacs.d"
+  export PRELUDE_URL="git@github.com:montchr/.emacs.d.git"
+
+  curl -L https://github.com/bbatsov/prelude/raw/master/utils/installer.sh | sh
+  ln -s "$DOTFILES/emacs/emacs.d" "$emacs_dir"
 fi
