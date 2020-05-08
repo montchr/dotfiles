d6b1bfc4766cdfd690748fcde200c4100be90b70
Chris Montgomery <chris@alley.co>
2020-05-08 12:27:44 -0400

    Use Homebrew bash installer

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -19,10 +19,11 @@ then
   # Install the correct homebrew for each OS type
   if test "$(uname)" = "Darwin"
   then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
   then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+    echo "  Linux? Eh. Sorry."
+    # ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
   fi
 fi
 
