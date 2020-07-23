1dd5801da478a69938c7431cf5fa21581c28bbaa
Chris Montgomery <chris@montchr.io>
2020-07-23 21:44:12 +0000

    Remove a comment and a bad script header

--- beets/install.sh
+++ beets/install.sh
@@ -24,9 +24,6 @@ pip install pylast
 # Beets: FetchArt
 pip install requests
 # Beets: Chromaprint/Acoustid
-# Note that there are further steps that must be taken that can't
-# be automated as easily; check out the documentation
-# http://beets.readthedocs.io/en/v1.4.5/plugins/chroma.html
 pip install pyacoustid
 
 if [ ! -d "$HOME/.config/beets" ] && [ ! -L "$HOME/.config/beets" ]; then
