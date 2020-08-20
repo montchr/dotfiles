fff096c71702e3aebc3107fb6b79a6d55d72e8bd
Chris Montgomery <chris@montchr.io>
2020-08-20 00:03:43 -0400

    install pyenv deps without confirmation

--- python/install.sh
+++ python/install.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 
 # pyenv dependencies on Linux
 if [ "$(uname -s)" == "Linux" ]; then
-  sudo apt-get update; sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+	sudo apt-get update
+	sudo apt-get install -y --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 fi
 
 # Install the versions of python listed in $DOTFILES/.python-version
