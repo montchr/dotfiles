ae891e86ff229cb5005f566c0af829eab11c0aec
Chris Montgomery <chris@montchr.io>
2020-11-27 18:31:55 -0500

    Offload karabiner and starship

--- karabiner/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ "$(uname -s)" == "Darwin" ]; then
-  if [ ! -d "$HOME/.config/karabiner" ] && [ ! -L "$HOME/.config/karabiner" ]; then
-  	ln -s "$DOTFILES/karabiner" "$HOME/.config/karabiner"
-  fi
-fi
